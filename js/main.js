4512b5d39ce94d5f8fd19022f9cbdf4ccdfb6c81
emilycopp <emily.copp@bell.net>
2016-04-07 17:40:06 -0400

    random star on refresh

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var $banner = $('.banner');
+var bannerWidth = $banner.outerWidth();
+var bannerHeight = $banner.outerHeight();
+var i = 0;
+var totalStars = 75;
+var $newStar;
+
+for (i = 0; i < totalStars; i++) {
+  $newStar = $('<div>');
+  $newStar.addClass('star');
+
+  $newStar.css({
+    top: Math.random() * bannerHeight,
+    left: Math.random() * bannerWidth,
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')',
+    opacity: Math.random()
+  });
+
+  $banner.append($newStar);
+}
